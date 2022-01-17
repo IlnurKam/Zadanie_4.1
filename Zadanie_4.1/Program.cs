8bfe350c2fe8f62b593ecdb69b9a92d7955dab66
kamal673 <kamal673@yandex.ru>
2022-01-17 19:21:53 +0300

    Добавьте файлы проекта.

---
+++ Zadanie_4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0, e = 1;
+            Console.Write("N=");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+                if (e <= n * 2)
+                {
+                    s += e;
+                    Console.WriteLine(s.ToString());
+                    e += 2;
+                }
+            Console.ReadKey();
+        }
+    }
+}
